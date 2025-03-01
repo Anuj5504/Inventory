02de7f773cdc2c899465c3497f9b8229e0b14d84
Anuj5504 <anujjadhav0505@gmail.com>
2025-03-01 13:33:31 +0530

    setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React,{ useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='text-red-500 '>Hii</div>
+        
+    </>
+  )
+}
+
+export default App
